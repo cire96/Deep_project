e4771e30d723ab1e7c0a62f5b0e5233bd7df4990
Eric Lind <ericl3@kth.se>
2020-04-28 15:09:25 +0200

    init

---
+++ VGGNet.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib as plt
+
+
+
+def plot_images(dataset, n_images, samples_per_image):
+    output = np.zeros((32 * n_images, 32 * samples_per_image, 3))
+
+    row = 0
+    for images in dataset.repeat(samples_per_image).batch(n_images):
+        output[:, row*32:(row+1)*32] = np.vstack(images.numpy())
+        row += 1
+
+    plt.figure()
+    plt.imshow(output)
+    plt.show()
+
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    featurewise_center=True,
+    featurewise_std_normalization=True,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True,
+    rescale=1.0/255.0)#validation_split=0.1
+
+
+num_classes=10
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+datagen.fit(x_train)
+
+
+
