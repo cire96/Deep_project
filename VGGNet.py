f066d1b692d299895b4cb57dafbd4ea2a0b12159
Eric Lind <ericl3@kth.se>
2020-04-29 15:00:00 +0200

    Let everything  burn

--- VGGNet.py
+++ VGGNet.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+
+
 
 
 
@@ -17,21 +20,61 @@ def plot_images(dataset, n_images, samples_per_image):
     plt.imshow(output)
     plt.show()
 
+
 datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1.0/255.0,
     featurewise_center=True,
     featurewise_std_normalization=True,
     rotation_range=20,
     width_shift_range=0.2,
     height_shift_range=0.2,
-    horizontal_flip=True,
-    rescale=1.0/255.0)#validation_split=0.1
+    horizontal_flip=True)
+test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+model= keras.Sequential()
+model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(10, activation='softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
+
+EPOCHS = 3
+BS = 100
 
 
 num_classes=10
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
 y_train = tf.keras.utils.to_categorical(y_train, num_classes)
 y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+
 datagen.fit(x_train)
+test_datagen.fit(x_test)
+
+history = model.fit(datagen.flow(x_train, y_train, batch_size=BS),
+    validation_data=(x_test, y_test),
+	steps_per_epoch=len(x_train) // BS,
+	epochs=EPOCHS)
+
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
 
 
 
