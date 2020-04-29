be614c62a036446a2b49a5572ad3a1c5ca94bd8f
Marcus Jirwe <jirwe@kth.se>
2020-04-29 17:24:19 +0200

    Something funny

--- VGGNet.py
+++ VGGNet.py
@@ -5,12 +5,9 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.layers import BatchNormalization, Dropout
+from tensorflow.keras import regularizers
 
-
-
-
-
-
+# A function used for plotting an image. 
 def plot_images(dataset, n_images, samples_per_image):
     output = np.zeros((32 * n_images, 32 * samples_per_image, 3))
 
@@ -23,113 +20,125 @@ def plot_images(dataset, n_images, samples_per_image):
     plt.imshow(output)
     plt.show()
 
-
-datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+# A ImageDataGenerator allowing dynamic data augmentation. 
+# Data augmentation includes rescaling the images, normalising the data, rotation of images, shifting images, and zooming.
+# Zoom range and fill mode are new
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1.0/255.0,
     samplewise_center=True,
     samplewise_std_normalization=True,
     rotation_range=20,
     width_shift_range=0.2,
     height_shift_range=0.2,
+    zoom_range = 0.2,
+    fill_mode = 'nearest',
     horizontal_flip=True)
 
-test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+# Same thing but for the test data. Only thing done here is normalising and rescaling.
+test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    samplewise_center=True,
+    samplewise_std_normalization=True)
 
+
+# Define a simple sequential model, allowing us to stack layers on top of each other.
 model = Sequential()
 
-# Test model:
-'''
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(10, activation='softmax'))
-model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
-'''
-
 # Simplified VGGNet without any residuals:
+lamda = 5e-4 # The parameter for weight decay regularisation.
 
-model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(32, 32, 3), padding = 'same'))
+# Because the model is very deep, we apply batch normalisation and dropout, which seemed to improve generalisation.
+
+# Reference for structure of VGGNet Model:
+# Karen Simonyan and Andrew Zisserman. Very deep convolutional networks for large-scale image recognition. arXiv preprint arXiv:1409.1556, 2014.
+
+# We chose to mimic configuration D. However, only one fully connected layer was used as recommended by examiner.
+
+# Input shape as defined by CIFAR-10 images.
+model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(32, 32, 3), padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
 model.add(BatchNormalization())
-model.add(Dropout(0.3))
+#model.add(Dropout(0.3))
 
-model.add(Conv2D(64, (3, 3), activation='relu', padding = 'same'))
-model.add(BatchNormalization())
-
-model.add(MaxPooling2D(pool_size = (2,2), strides=(2,2)))
-
-model.add(Conv2D(128, (3, 3), activation='relu', padding = 'same'))
-model.add(BatchNormalization())
-model.add(Dropout(0.4))
-
-model.add(Conv2D(128, (3, 3), activation='relu', padding = 'same'))
-model.add(BatchNormalization())
-
-model.add(MaxPooling2D(pool_size = (2,2), strides=(2,2)))
-
-model.add(Conv2D(256, (3, 3), activation='relu', padding = 'same'))
-model.add(BatchNormalization())
-model.add(Dropout(0.4))
-
-model.add(Conv2D(256, (3, 3), activation='relu', padding = 'same'))
-model.add(BatchNormalization())
-model.add(Dropout(0.4))
-
-model.add(Conv2D(256, (3, 3), activation='relu', padding = 'same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
 model.add(BatchNormalization())
 
 model.add(MaxPooling2D(pool_size = (2,2), strides=(2,2)))
 
-model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
 model.add(BatchNormalization())
-model.add(Dropout(0.4))
+#model.add(Dropout(0.4))
 
-model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
 model.add(BatchNormalization())
-model.add(Dropout(0.4))
 
-model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same'))
+model.add(MaxPooling2D(pool_size = (2,2), strides=(2,2)))
+
+model.add(Conv2D(256, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
+model.add(BatchNormalization())
+#model.add(Dropout(0.4))
+
+model.add(Conv2D(256, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
+model.add(BatchNormalization())
+#model.add(Dropout(0.4))
+
+model.add(Conv2D(256, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
+model.add(BatchNormalization())
+
+model.add(MaxPooling2D(pool_size = (2,2), strides=(2,2)))
+
+model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
+model.add(BatchNormalization())
+#model.add(Dropout(0.4))
+
+model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
+model.add(BatchNormalization())
+#model.add(Dropout(0.4))
+
+model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
+model.add(BatchNormalization())
+#model.add(Dropout(0.4))
+
+model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
+model.add(BatchNormalization())
+#model.add(Dropout(0.4))
+
+model.add(Conv2D(512, (3, 3), activation='relu', padding = 'same', kernel_regularizer = regularizers.l2(lamda)))
 model.add(BatchNormalization())
 
 model.add(MaxPooling2D(pool_size = (2,2), strides=(2,2)))
 model.add(Dropout(0.5))
 
 model.add(Flatten())
-model.add(Dense(4096, activation='relu'))
+model.add(Dense(4096, activation='relu', kernel_regularizer = regularizers.l2(lamda)))
 model.add(BatchNormalization())
 model.add(Dropout(0.5))
 
 model.add(Dense(10, activation='softmax'))
 model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
+# End of model.
 
-
-
-
+# Batch size defined in article, with some epoch number. Number of classes defined by CIFAR-10.
 EPOCHS = 100
-BS = 128
-
-
+BS = 256
 num_classes=10
+
+# Download CIFAR-10 from keras. Only done once per machine.
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
+# Change the labels to one-hot encoding.
 y_train = tf.keras.utils.to_categorical(y_train, num_classes)
 y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
-
-#datagen.fit(x_train)
-#test_datagen.fit(x_test)
-
-history = model.fit(datagen.flow(x_train, y_train, batch_size=BS),
+# Save the history of the training in a dictionary. This is the accuracy and loss over the epochs (training & test). 
+history = model.fit(train_datagen.flow(x_train, y_train, batch_size=BS),
     validation_data=test_datagen.flow(x_test, y_test, batch_size=BS),
 	steps_per_epoch=len(x_train) // BS, epochs=EPOCHS)
 
-'''
-history = model.fit(datagen.flow(x_train, y_train, batch_size=BS),
-    validation_data=(x_test, y_test),
-	steps_per_epoch=len(x_train) // BS, epochs=EPOCHS)
-'''
 
-print(history.history.keys())
-# summarize history for accuracy
+# Plot the accuracy for training and test set over the epochs.
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
 plt.title('model accuracy')
@@ -137,7 +146,7 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='best')
 plt.show()
-# summarize history for loss
+# Plot the loss for training and test set over the epochs.
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
